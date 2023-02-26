3250f1bc6a79a4f572ddcc278f49485d06234e27
Assppex <joom361@gmail.com>
2023-02-26 20:24:33 +0300

    added functions to append to freelist and remove from freelist and almost everything connected with freelist except search of freeblocks, TODO: split and coalscene (обьединение блоков) + сама функция алока и free

--- custom_malloc/custom_malloc.c
+++ custom_malloc/custom_malloc.c
@@ -39,8 +39,10 @@ typedef struct metadata{
     size_t size; // размер всего блока header + payload + footer
 }metadata_t;
 
+//тут все аналогично
 typedef struct metadata_end{
     int if_available;
+    size_t size;
 }metadata_end_t;
 /*
     В слуае свободного блока вмсто полезной нагрузки в память мы будем помещать указатель на следующий 
@@ -52,8 +54,9 @@ typedef struct metadata_end{
 typedef struct freelist{
     struct freelist *prev;
     struct freelist *next;
-}free_list_t;
+}freelist_t;
 
+freelist_t* freelist_first_item = NULL;
 /*
     Мы будем использовать для аллокации mmap,а не sbrk - возвращает систем брейк, 
     т.е. крайнюю границу размеченной для процесса памяти,brk - просто смещает эту границу (т.е.
@@ -74,6 +77,8 @@ typedef struct freelist{
 */
 #define HEADER_SZ sizeof(metadata_t)
 #define FOOTER_SZ sizeof(metadata_end_t)
+#define IN_USE 1
+#define VACANT 0
 //Функция для получения указателя на полезную нагрузку в блоке (фрагменте памяти), указатель на начало которого - ptr
 //т.е ptr - указатель на header
 void* get_ptr_to_payload(void* ptr){
@@ -83,6 +88,109 @@ void* get_ptr_to_payload(void* ptr){
 void* get_ptr_to_footer(void* ptr){
     return ptr + HEADER_SZ + ((metadata_t*)ptr)->size - FOOTER_SZ;
 }
+/*
+    Функция, чтобы менять состояние блока (занят/свободен)
+
+    ptr - указатель на начало блока (header); acces_status - флаг доступа (свободен или занят)
+    дело в том, что выделяем мы память не взирая на то, какой тип данных нужно поместить туда
+    (любой аллокатор возвращает void*, который кастится в любой тип), т.е. зная указатель на весь блок
+    мы спокойно можем кастить его в структуры метаданных, потому что, например по адресу начала нашего блока
+    будет располагаться именно структура metadata_t, а подходя к концу выделенного блока, и зная о том, что
+    в конце дожен быть footer (metadata_end_t) мы получии с помощью специально функции указатель на то место,где
+    должен быть footer и прокастим его в вышеуказанный тип структуры metada_end_t, тем самым ,после каста мы сможем получить
+    доступ к элементам footer,по указателю на начало всего блока и имеющего вообще тип void*.
+*/
+void set_tag_of_access(void* ptr,int access_status){
+    ((metadata_t*)ptr)->if_available = access_status;
+    metadata_end_t* footer = (metadata_end_t*)get_ptr_to_footer(ptr);
+    footer->if_available = access_status;
+    footer->size = ((metadata_t*)ptr)->size;
+
+}
+/*
+    Далее пойдет функция для работы с фрилистом: при новом выделении памяти, мы сначала будем проверять, а не
+    существует ли блока, который имеет примерно такой же размер, какой запрашивается пользователем, и если такой блок
+    уже имеется, то смысла запрашивать новую память у системы нет, это сэкономит ресурсы ОС. ptr - указатель на начало блока
+*/
+int check_size_of_free_list_instance(void* ptr){
+    int size = ((metadata_t*)ptr)->size;
+    return size;
+}
+/*
+    Далее естественно будут нужны функции для добавления фрагментов памяти во фрилист и, соответственно, удаления
+    их от туда, ptr - указатель на начало выделенного блока памяте (на его header)
+*/
+void add_to_free_list(void* ptr){
+    set_tag_of_access(ptr,VACANT);
+    freelist_t new_freelist_block;
+    /*
+        Получим указатель на саму память в блоке предназначенную для полезной нагрузки
+        туда мы и поместим структуру данных фрилиста, где содержаться указатели на следующий свободный
+        блок и предыдущий
+    */
+    freelist_t* new_freelist_block_ptr = get_ptr_to_payload(ptr);
+    if(!freelist_first_item){
+        //Если рассматриваемый блок памяти первый, помещенный во фрилист, то не следующего, не предыдущего блока не будет
+        freelist_first_item = new_freelist_block_ptr;
+        new_freelist_block_ptr->prev = NULL;
+        new_freelist_block_ptr->next = NULL;
+    }
+    else{
+        /*
+        freelist работает по принципо FIFO (First in first out), 
+        т.е. первый попавший во фрилст блок будет в начале,т.е. каждый новый блок мы записываем в начало.
+        FIFO предпочтительнее поскольку с точки зрения процессора последний высвобожденный блок еще находится в кеше
+        то есть, с ним коммуникация будеь быстрее, чем с блоком, который был освобожден до этого.
+        */
+       new_freelist_block_ptr->next = freelist_first_item;
+       new_freelist_block_ptr->prev = NULL;
+       freelist_first_item->prev = new_freelist_block_ptr;
+       //ну и теперь первый элемент в двусвязном списке сменился, то есть надо его сменить
+       freelist_first_item = new_freelist_block_ptr;
+    }
+}
+/*
+    Тут мы удаляем из фрилиста, то есть имеем дело со свободными блоками 
+    (т.е знаем что в памяти для полезной нагрузки) содержится структура freelist_t
+    , надо сменить линковку (то есть переобозначить по новой указатели)
+*/
+void delete_from_free_list(void* ptr){
+    set_tag_of_access(ptr,IN_USE);
+    freelist_t* block_to_be_freed = (freelist_t*)get_ptr_to_payload(ptr);
+
+    freelist_t* next_free_block = block_to_be_freed ->next;
+    freelist_t* prev_free_block = block_to_be_freed ->prev;
+
+    if(!prev_free_block){
+        if(!next_free_block){
+            //То есть фрилист состоит только из одного элмента, с которым мы и имеем дело
+            freelist_first_item = NULL;
+        }
+        else{
+            /*
+                То есть мы имеем дело с первым блоком во фрилисте
+            */
+            freelist_first_item = next_free_block;
+            /*
+                Ну и поскольку теперь следующий блок - это первый блок во фрилисте,
+                 то у него не должо быть предыдущего.
+            */
+            (freelist_first_item)->prev = NULL;
+        }
+        
+    }
+    else{
+        if(!next_free_block){
+            //То есть если нет следующего блока во фрилисте (имеем дело с последним блоком фрилиста)
+            prev_free_block->next = NULL;
+        }
+        else{
+            next_free_block->prev = prev_free_block;
+            prev_free_block->next = next_free_block;
+        }
+    }
+}
+
 int main (void){
     metadata_t a;
     metadata_end_t b;
